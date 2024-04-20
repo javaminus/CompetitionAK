8e484bde64b840597499338920c3ea680eac2877
Minus <3292158907@qq.com>
2024-04-20 13:21:46 +0800

    2024/4/20 算法并不是最难的，因为这事逻辑。 真正困难的地方，是贪婪思维下的隐性决策，那是无法细节预测和探索的。

--- src/Solution.java
+++ src/Solution.java
@@ -1,52 +1,26 @@
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Arrays;
 
 class Solution {
-    public int[] findOriginalArray(int[] changed) {
-        int n = changed.length;
-        if (n % 2 == 1) {
-            return new int[0];
-        }
-        int[] ans = new int[n / 2];
-        int ansId = 0;
-        HashMap<Integer, Integer> cnt = new HashMap<>();
-        for (int x : changed) {
-            cnt.merge(x, 1, Integer::sum);
-        }
-        int cnt0 = cnt.getOrDefault(0, 0);
-        if (cnt0 % 2 == 1) {
-            return new int[0];
-        }
-        cnt.remove(0);
-        HashSet<Integer> done = new HashSet<>();
-        for (int x : cnt.keySet()) {
-            if (done.contains(x) || x % 2 == 0 && cnt.containsKey(x / 2)) {
-                continue;
-            }
-            while (cnt.containsKey(x)) {
-                done.add(x);
-                int cntX = cnt.get(x);
-                int cnt2x = cnt.getOrDefault(x * 2, 0);
-                if (cntX > cnt2x) {
-                    return new int[0];
-                }
-                for (int i = 0; i < cntX; i++) {
-                    if (ansId > ans.length) {
-                        return new int[0];
-                    }
-                    ans[ansId++] = x;
-                }
-                if (cnt2x > cntX) {
-                    cnt.put(x * 2, cnt2x - cntX);
-                    x *= 2;
-                }else{
-                    done.add(x * 2);
-                    x *= 4;
-                }
+    private static final int Mod = (int) (1e9 + 7);
 
+    public int numWays(String[] words, String target) {
+        int m = words[0].length(), n = target.length();
+        int[][] dp = new int[n + 1][m + 1]; // dp[i][j]表示从前j个字母选择构成target[:i]的方案数
+        Arrays.fill(dp[0], 1); // 构成空字串的方案数，都是1
+        int[][] cnt = new int[m + 1][26]; // cnt[i][j]表示前i个字符构成字符（j+'a'）的方案数
+        for (String s : words) {
+            for (int i = 0; i < m; i++) {
+                cnt[i + 1][s.charAt(i) - 'a']++;
             }
         }
-        return ans;
-
+        for (int i = 1; i <= n; i++) {
+            int c = target.charAt(i - 1) - 'a';
+            int t = m - (n - i);
+            for (int j = i; j <= t; j++) {
+                long ans = (long) cnt[j][c] * dp[i - 1][j - 1] + dp[i][j - 1];
+                dp[i][j] = (int) (ans % Mod);
+            }
+        }
+        return dp[n][m];
     }
 }
--- src/com/Java_Template/dp/linear_dp/probemImpl.java
+++ src/com/Java_Template/dp/linear_dp/probemImpl.java
@@ -171,4 +171,212 @@ public class probemImpl {
         }
         return dp[m][n];
     }
+
+    /*  115. 不同的子序列
+        给你两个字符串 s 和 t ，统计并返回在 s 的 子序列 中 t 出现的个数，结果需要对 109 + 7 取模。*/
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length(), Mod = (int) 1e9 + 7;
+        int[][] dp = new int[m + 1][n + 1]; // dp[i][j]表示s[:i]中有多少个与t[:j]相匹配
+        // 初始化
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i + 1][j + 1] = dp[i][j + 1]; // 不选s[i]
+                if (s.charAt(i) == t.charAt(j)) {
+                    dp[i + 1][j + 1] = (dp[i + 1][j + 1] + dp[i][j]) % Mod;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /*  1035. 不相交的线
+        在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+        现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足：
+         nums1[i] == nums2[j]
+        且绘制的直线不与任何其他连线（非水平线）相交。
+        请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+        以这种方法绘制线条，并返回可以绘制的最大连线数。*/
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 不选
+                dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                if (nums1[i] == nums2[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /*  1458. 两个子序列的最大点积
+        给你两个数组 nums1 和 nums2 。
+        请你返回 nums1 和 nums2 中两个长度相同的 非空 子序列的最大点积。
+        数组的非空子序列是通过删除原数组中某些元素（可能一个也不删除）后剩余数字组成的序列，但不能改变数字间相对顺序。比方说，[2,3,5] 是 [1,2,3,4,5] 的一个子序列而 [1,5,3] 不是。*/
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE / 2);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i + 1][j + 1] = Math.max(Math.max(Math.max(dp[i + 1][j], dp[i][j + 1]), dp[i][j] + nums1[i]*nums2[j]),nums1[i]*nums2[j]);
+            }
+        }
+        return dp[m][n];
+    }
+
+    /*  1092. 最短公共超序列
+        给你两个字符串 str1 和 str2，返回同时以 str1 和 str2 作为 子序列 的最短字符串。如果答案不止一个，则可以返回满足条件的 任意一个 答案。
+        如果从字符串 t 中删除一些字符（也可能不删除），可以得到字符串 s ，那么 s 就是 t 的一个子序列。*/
+
+/*    public String shortestCommonSupersequence(String s, String t) { // 递归超时
+        if (s.isEmpty()) {
+            return t;
+        }
+        if (t.isEmpty()) {
+            return s;
+        }
+        String s1 = s.substring(0, s.length() - 1);
+        String t1 = t.substring(0, t.length() - 1);
+        char x = s.charAt(s.length() - 1);
+        char y = t.charAt(t.length() - 1);
+        if (x == y) {
+            return shortestCommonSupersequence(s1, t1) + x;
+        }else{
+            String ans1 = shortestCommonSupersequence(s1, t);
+            String ans2 = shortestCommonSupersequence(s, t1);
+            if (ans1.length() < ans2.length()) {
+                return ans1 + x;
+            }else{
+                return ans2 + y;
+            }
+        }
+    }*/
+    /*  1:1翻译成递推：爆内存
+    private String s, t;
+    private String[][] memo;
+    public String shortestCommonSupersequence(String s, String t) {
+        this.s = s;
+        this.t = t;
+        memo = new String[s.length()][t.length()];
+        return dfs(s.length() - 1, t.length() - 1);
+    }
+
+    private String dfs(int i, int j) {
+        if (i < 0) {
+            return t.substring(0, j + 1);
+        }
+        if (j < 0) {
+            return s.substring(0, i + 1);
+        }
+        if (memo[i][j] != null) {
+            return memo[i][j];
+        }
+        if (s.charAt(i) == t.charAt(j)) {
+            return dfs(i - 1, j - 1) + s.charAt(i);
+        }
+        // 不相同
+        String ans1 = dfs(i - 1, j);
+        String ans2 = dfs(i, j - 1);
+        if (ans1.length() < ans2.length()) {
+            return memo[i][j] = ans1 + s.charAt(i);
+        }else{
+            return memo[i][j] = ans2 + t.charAt(j);
+        }
+    }*/
+    // 如果只求最短公共超序列的长度，那么递归返回的是一个整数而不是字符串，这样就只需要 O(nm) 的时间和空间，这是可以接受的。
+    // 问题在于，我们需要构造一个具体的最短公共超序列。
+    /*
+    private String s, t;
+    private int[][] memo; // 存储长度
+    public String shortestCommonSupersequence(String s, String t) {
+        this.s = s;
+        this.t = t;
+        memo = new int[s.length()][t.length()];
+        return makeAns(s.length() - 1, t.length() - 1);
+    }
+
+    private int dfs(int i, int j) {
+        if (i < 0) {
+            return j + 1;
+        }
+        if (j < 0) {
+            return i + 1;
+        }
+        if (memo[i][j] > 0) {
+            return memo[i][j];
+        }
+        if (s.charAt(i) == t.charAt(j)) {
+            return memo[i][j] = dfs(i - 1, j - 1) + 1;
+        }else{
+            return memo[i][j] = Math.min(dfs(i - 1, j), dfs(i, j - 1)) + 1;
+        }
+    }
+
+    private String makeAns(int i, int j) {
+        if (i < 0) {
+            return t.substring(0, j + 1);
+        }
+        if (j < 0) {
+            return s.substring(0, i + 1);
+        }
+        if (s.charAt(i) == t.charAt(j)) {
+            return makeAns(i - 1, j - 1) + s.charAt(i);
+        }
+        if (dfs(i, j) == dfs(i - 1, j) + 1) {
+            return makeAns(i - 1, j) + s.charAt(i);
+        }else{
+            return makeAns(i, j - 1) + t.charAt(j);
+        }
+    }*/
+    public String shortestCommonSupersequence(String str1, String str2) { // 终解
+        char[] s = str1.toCharArray(), t = str2.toCharArray();
+        int m = s.length, n = t.length;
+        int[][] dp = new int[m + 1][n + 1]; // 长度dp数组
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s[i] == t[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                }else{
+                    dp[i + 1][j + 1] = Math.min(dp[i][j + 1], dp[i + 1][j]) + 1;
+                }
+            }
+        }
+        int len = dp[m][n];
+        char[] ans = new char[len];
+        for (int i = m - 1, j = n - 1, k = len - 1; ; ) { // 学会
+            if (i < 0) {
+                System.arraycopy(t, 0, ans, 0, j + 1); // 学会
+                break;
+            }
+            if (j < 0) {
+                System.arraycopy(s, 0, ans, 0, i + 1);
+                break;
+            }
+            if (s[i] == t[j]) {
+                ans[k--] = s[i--];
+                j--; // 相当于继续递归 makeAns(i - 1, j - 1)
+            } else if (dp[i + 1][j + 1] == dp[i + 1][j] + 1) {
+                ans[k--] = t[j--];
+            }else{
+                ans[k--] = s[i--];
+            }
+        }
+        return new String(ans);
+    }
+
+
 }
