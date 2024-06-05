705d8d7f53678c80538368a24259809ed8cd00fa
Minus <3292158907@qq.com>
2024-06-05 23:53:47 +0800

    2024/6/5 国优，真的没想到是这个结果

--- src/Solution.java
+++ src/Solution.java
@@ -1,50 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public int collectTheCoins(int[] coins, int[][] edges) {
-        int n = coins.length;
-        List<Integer> g[] = new ArrayList[n];
-        Arrays.setAll(g, e -> new ArrayList<>());
-        int[] deg = new int[n];
-        for (int[] e : edges) {
-            int x = e[0], y = e[1];
+    public int minimumTime(int n, int[][] relations, int[] time) {
+        List<Integer>[] g = new List[n];
+        Arrays.setAll(g, e -> new ArrayList<Integer>());
+        int[] indegree = new int[n];
+        for (int[] rela : relations) {
+            int x = rela[0] - 1, y = rela[1] - 1;
+            indegree[y]++;
             g[x].add(y);
-            g[y].add(x); // 建图
-            deg[x]++;
-            deg[y]++; // 统计每个节点的度数（邻居个数）
         }
-
-        int leftEdges = n - 1; // 剩余边数
-        // 拓扑排序，去掉没有金币的子树
-        Queue<Integer> q = new ArrayDeque<Integer>();
+        Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            if (deg[i] == 1 && coins[i] == 0) { // 没有金币的叶子
-                q.add(i);
+            if (indegree[i] == 0) {
+                queue.offer(i);
             }
         }
-        while (!q.isEmpty()) {
-            leftEdges--; // 删除节点到其父节点的边
-            for (int y : g[q.poll()]) {
-                if (--deg[y] == 1 && coins[y] == 0) { // 没有金币的叶子
-                    q.add(y);
-                }
-            }
-        }
-
-        // 再次拓扑排序
-        for (int i = 0; i < n; i++) {
-            if (deg[i] == 1 && coins[i] == 1) { // 有金币的叶子（判断 coins[i] 是避免把没有金币的叶子也算进来）
-                q.add(i);
-            }
-        }
-        leftEdges -= q.size(); // 删除所有叶子（到其父节点的边）
-        for (int x : q) { // 遍历所有叶子
+        int ans = 0;
+        int[] dp = new int[n]; // 表示完成任务i的最少用时
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            dp[x] += time[x];
+            ans = Math.max(ans, dp[x]);
             for (int y : g[x]) {
-                if (--deg[y] == 1) { // y 现在是叶子了
-                    leftEdges--; // 删除 y（到其父节点的边）
+                dp[y] = Math.max(dp[y], dp[x]);
+                if (--indegree[y] == 0) {
+                    queue.offer(y);
                 }
             }
         }
-        return Math.max(leftEdges * 2, 0);
+        return ans;
     }
 }
