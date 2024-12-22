bfa39ccfebc05ad4a66debeb625e64a8454d0ee4
javaminus <3292158907@qq.com>
2024-12-23 00:51:02 +0800

    2024/12/23

--- src/Solution.java
+++ src/Solution.java
@@ -1,63 +1,31 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
-    class Pair{
-        String son;
-        Double val;
-
-        public Pair(String son, Double val) {
-            this.son = son;
-            this.val = val;
+    public int minimumOperations(int[] nums) {
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        int n = nums.length;
+        for (int x : nums) {
+            cnt.merge(x, 1, Integer::sum);
         }
-    }
-    public double maxAmount(String initialCurrency, List<List<String>> pairs1, double[] rates1, List<List<String>> pairs2, double[] rates2) {
-        Map<String, List<Pair>> g = build(pairs1, rates1);
-        HashMap<String, Double> day1Amount = new HashMap<>();
-        init(initialCurrency, 1, g, day1Amount);
-        g = build(pairs2, rates2);
-        HashSet<String> vis = new HashSet<>();
-        for (Map.Entry<String, Double> e : day1Amount.entrySet()) {
-            vis.clear();
-            dfs(e.getKey(), e.getValue(), initialCurrency, g, vis);
-        }
-        return ans;
-    }
-
-    private double ans;
-    private boolean dfs(String x, double curAmount, String initialCurrency, Map<String, List<Pair>> g, Set<String> vis) {
-        if (x.equals(initialCurrency)) {
-            ans = Math.max(ans, curAmount);
-            return true;
-        }
-        vis.add(x);
-        if (!g.containsKey(x)) {
-            return false;
-        }
-        for (Pair p : g.get(x)) {
-            if (!vis.contains(p.son) && dfs(p.son, curAmount * p.val, initialCurrency, g, vis)) {
-                return true;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (cnt.size() == n - i) {
+                return res;
             }
-        }
-        return false;
-    }
-    
-    
-    private void init(String x, double amount, Map<String, List<Pair>> g, Map<String, Double> day1Amount) {
-        day1Amount.put(x, amount);
-        for (Pair p : g.get(x)) {
-            if (!day1Amount.containsKey(p.son)) {
-                init(p.son, amount * p.val, g, day1Amount);
+            res++;
+            if (i + 3 >= n) {
+                return res;
             }
+            for (int j = 0; j < 3; j++) {
+                Integer v = cnt.get(nums[i + j]) - 1;
+                if (v == 0) {
+                    cnt.remove(nums[i + j]);
+                }else{
+                    cnt.put(nums[i + j], v);
+                }
+            }
+            i += 2;
         }
-    }
-    private Map<String, List<Pair>> build(List<List<String>> pairs, double[] rates) {
-        HashMap<String, List<Pair>> g = new HashMap<>();
-        for (int i = 0; i < pairs.size(); i++) {
-            String x = pairs.get(i).get(0), y = pairs.get(i).get(1);
-            double r = rates[i];
-            g.computeIfAbsent(x, e -> new ArrayList<>()).add(new Pair(y, r));
-            g.computeIfAbsent(y, e -> new ArrayList<>()).add(new Pair(x, 1 / r));
-        }
-        return g;
+        return (n + 2) / 3;
     }
 }
