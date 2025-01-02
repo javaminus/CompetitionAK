67fa66900309181859d6d14136a53d8da74aafd4
javaminus <3292158907@qq.com>
2025-01-02 12:30:47 +0800

    2025/1/2

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,4 @@
-public class Main { public static void main(String[] args) { String s = "public class Main { public static void main(String[] args) { String s = %c%s%c; System.out.printf(s, 34, s, 34); } }"; System.out.printf(s, 34, s, 34); } }
+public class Main { public static void main(String[] args) { 
+    String s = "public class Main { public static void main(String[] args) { String s = %c%s%c; System.out.printf(s, 34, s, 34); } }";
+    System.out.printf(s, 34, s, 34); } 
+}
--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,82 @@
-import java.util.Arrays;
+class MyCalendar {
+    Node root = new Node();
+    int N = (int) 1e9;
+    public MyCalendar() {
+        // update(root, 0, N, 0, N, 0);
+    }
 
-class Solution {
-    public long minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
-        Arrays.sort(horizontalCut);
-        Arrays.sort(verticalCut);
-        long ans = 0;
-        int i = m - 2, j = n - 2, cntH = 1, cntV = 1;
-        while (i >= 0 && j >= 0) {
-            if (horizontalCut[i] > verticalCut[j]) {
-                ans += (long) horizontalCut[i] * cntV;
-                cntH++;
-                i--;
-            }else{
-                ans += (long) verticalCut[j] * cntH;
-                cntV++;
-                j--;
-            }
+    public boolean book(int start, int end) {
+        update(root, 0, N, start, end - 1, 1);
+        int ans = query(root, 0, N, start, end - 1);
+        if (ans < 2) {
+            return true;
+        }else{
+            update(root, 0, N, start, end - 1, -1);
+            return false;
         }
-        while (i >= 0) {
-            ans += (long) horizontalCut[i] * cntV;
-            i--;
+    }
+
+    class Node{
+        Node leftNode, rightNode;
+        int val, add;
+    }
+
+    void update(Node node, int leftChild, int rightChild, int left, int right, int delta) {
+        if (left <= leftChild && right >= rightChild) {
+            node.val += delta;
+            node.add += delta;
+            return;
         }
-        while (j >= 0) {
-            ans += (long) verticalCut[j] * cntH;
-            j--;
+        pushdown(node);
+        int mid = leftChild + (rightChild - leftChild) / 2;
+        if (left <= mid) {
+            update(node.leftNode, leftChild, mid, left, right, delta);
+        }
+        if (right > mid) {
+            update(node.rightNode, mid + 1, rightChild, left, right, delta);
+        }
+        pushup(node);
+    }
+
+    int query(Node node, int leftChild, int rightChild, int left, int right) {
+        if (left <= leftChild && right >= rightChild) {
+            return node.val;
+        }
+        int mid = leftChild + (rightChild - leftChild) / 2, ans = 0;
+        if (left <= mid) {
+            ans = query(node.leftNode, leftChild, mid, left, right);
+        }
+        if (right > mid) {
+            ans = Math.max(ans, query(node.rightNode, mid + 1, rightChild, left, right));
         }
         return ans;
     }
+
+    void pushdown(Node node) {
+        if (node.leftNode == null) {
+            node.leftNode = new Node();
+        }
+        if (node.rightNode == null) {
+            node.rightNode = new Node();
+        }
+        if (node.add != 0) {
+            int add = node.add;
+            node.leftNode.val += add;
+            node.rightNode.val += add;
+            node.leftNode.add += add;
+            node.rightNode.add += add;
+            node.add = 0;
+        }
+    }
+
+    void pushup(Node node) {
+        node.val = Math.max(node.leftNode.val, node.rightNode.val);
+    }
+
 }
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
