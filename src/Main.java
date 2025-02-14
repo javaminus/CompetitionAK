9f1bd799e677b7ecd994c26c3be96cf0b400890f
javaminus <3292158907@qq.com>
2025-02-14 17:42:09 +0800

    2025/2/14

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,250 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Main {
-    static final long MOD = 1000000007;
-    public static void main(String[] args) throws Exception{
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String line = in.readLine().trim();
-        String[] parts = line.split("],");
-        if(parts.length < 2){
-            System.out.println(0);
+    private final static int INF = Integer.MAX_VALUE / 2;
+    private final static int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    static class Read {
+        BufferedReader bf;
+        StringTokenizer st;
+        BufferedWriter bw;
+
+        public Read() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+            st = new StringTokenizer("");
+            bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public String nextLine() throws IOException {
+            return bf.readLine();
+        }
+
+        public String next() throws IOException {
+            while (!st.hasMoreTokens()) {
+                st = new StringTokenizer(bf.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        public byte nextByte() throws IOException {
+            return Byte.parseByte(next());
+        }
+
+        public short nextShort() throws IOException {
+            return Short.parseShort(next());
+        }
+
+        public BigInteger nextBigInteger() throws IOException {
+            return new BigInteger(next());
+        }
+
+        public void println(int a) throws IOException {
+            bw.write(String.valueOf(a));
+            bw.newLine();
             return;
         }
-        String numStr = parts[0].substring(1);
-        String[] numsStr = numStr.split(",");
-        int n = numsStr.length;
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(numsStr[i].trim());
+
+        public void print(int a) throws IOException {
+            bw.write(String.valueOf(a));
+            return;
         }
-        String colorStr = parts[1].trim();
-        if(colorStr.startsWith("\"") && colorStr.endsWith("\""))
-            colorStr = colorStr.substring(1, colorStr.length()-1);
-        int preRedMod = 0;
-        int countOdd = 0;
-        int countEven = 0;
-        for(int i = 0; i < n; i++){
-            char c = colorStr.charAt(i);
-            if(c == 'R'){
-                preRedMod = (preRedMod + arr[i]) % 2;
-            } else {
-                if(arr[i] % 2 == 0){
-                    countEven++;
-                } else {
-                    countOdd++;
-                }
-            }
+
+        public void println(String a) throws IOException {
+            bw.write(a);
+            bw.newLine();
+            return;
         }
-        long waysEven, waysOdd;
-        long evenSubsets = modPow(2, countEven); // 偶数选择的个数
-        if(countOdd > 0){
-            waysEven = modPow(2, countOdd - 1); // 有一个必选，然后剩下n - 1个选与不选
-            waysOdd = waysEven;
-        } else {
-            waysEven = 1;
-            waysOdd = 0;
+
+        public void print(String a) throws IOException {
+            bw.write(a);
+            return;
         }
-        int required = (2 - preRedMod) % 2;
-        long whiteWays = (required == 0 ? waysEven : waysOdd);
-        long ans = (evenSubsets * whiteWays) % MOD;
-        System.out.println(ans);
+
+        public void println(long a) throws IOException {
+            bw.write(String.valueOf(a));
+            bw.newLine();
+            return;
+        }
+
+        public void print(long a) throws IOException {
+            bw.write(String.valueOf(a));
+            return;
+        }
+
+        public void println(double a) throws IOException {
+            bw.write(String.valueOf(a));
+            bw.newLine();
+            return;
+        }
+
+        public void print(double a) throws IOException {
+            bw.write(String.valueOf(a));
+            return;
+        }
+
+        public void print(BigInteger a) throws IOException {
+            bw.write(a.toString());
+            return;
+        }
+
+        public void print(char a) throws IOException {
+            bw.write(String.valueOf(a));
+            return;
+        }
+
+        public void println(char a) throws IOException {
+            bw.write(String.valueOf(a));
+            bw.newLine();
+            return;
+        }
     }
-    static long modPow(long base, int exp){
-        long res = 1;
-        base = base % MOD;
-        while(exp > 0){
-            if((exp & 1) == 1) res = (res * base) % MOD;
-            base = (base * base) % MOD;
-            exp >>= 1;
+
+    static class Pair<T, U> {
+        T fir;
+        U sec;
+        public Pair(T fir, U sec) {
+            this.fir = fir;
+            this.sec = sec;
+        }
+    }
+
+    private static long qpow(long a, long b, long p) {
+        long res = 1L;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                res = (res * a) % p;
+            }
+            a = a * a % p;
+            b >>= 1;
         }
         return res;
     }
+
+    private static long sqrt(long N) { // 二分查找快速开方
+        long lo = 1;
+        long hi = N;
+        long ans = 0;
+        while(lo <= hi) {
+            long mid = (lo + hi) / 2;
+            if (mid <= N / mid) {
+                ans = mid;
+                lo = mid + 1;
+            }  else {
+                hi = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private static void reverse(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l <= r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+
+    private static void reverse(int[] s) {
+        int l = 0, r = s.length - 1;
+        while (l <= r) {
+            int tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+
+    private static void reverse(long[] s) {
+        int l = 0, r = s.length - 1;
+        while (l <= r) {
+            long tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+
+    static Read sc = new Read();
+    private static final int Mod = (int) 1e9 + 7;
+    private static int T = 1;
+
+    public static void main(String[] args) throws IOException {
+        // int T = sc.nextInt();
+        while (T-- > 0) {
+            solve();
+            // sc.bw.flush();
+        }
+        sc.bw.flush();
+        sc.bw.close();
+    }
+
+    private static String[] ss;
+    private static String s;
+    private static char[] cs;
+    private static List<Integer>[] g;
+    private static int m, n;
+
+
+    private static void solve() throws IOException {
+        ss = sc.nextLine().split("]");
+        s = ss[0].substring(1);
+        String[] ss1 = s.split(",");
+        n = ss1.length;
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(ss1[i]);
+        }
+        cs = ss[1].substring(2, ss[1].length() - 1).toCharArray();
+        int op = 0;
+        for (int i = 0; i < n; i++) {
+            if (cs[i] == 'R') {
+                op += nums[i];
+                op = op % 2;
+            }
+        }
+        int[][] dp = new int[n][2]; // 表示到第i位，余数为j的方案数
+        if (cs[0] == 'W') {
+            dp[0][0] = 1;
+        }
+        if ((nums[0] & 1) == 0) {
+            dp[0][0] += 1;
+        }else{
+            dp[0][1] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                dp[i][j] = (dp[i - 1][j] + (cs[i] == 'W' ? dp[i - 1][(j + nums[i]) % 2] : 0)) % Mod;
+            }
+        }
+        sc.println(dp[n - 1][0]);
+    }
+    
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,51 +1,10 @@
-import java.util.Deque;
-import java.util.LinkedList;
-
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        int m = matrix.length;
-        if (m == 0) {
-            return 0;
-        }
-        int n = matrix[0].length;
-        int[][] left = new int[m][n]; // left[i][j]就是第i行中，第j列的柱状高度
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == '1') {
-                    left[i][j] = (j == 0 ? 0 : left[i][j - 1]) + 1; // 按列累加
-                }
+    public int makeTheIntegerZero(int num1, int num2) {
+        for (long k = 1; k <= num1 - num2 * k; k++) {
+            if (k >= Long.bitCount(num1 - num2 * k)) {
+                return (int) k;
             }
         }
-
-        int ret = 0;
-        for (int j = 0; j < n; j++) { // 对于每一列，使用基于柱状图的方法 https://leetcode.cn/problems/largest-rectangle-in-histogram/solutions/266844/zhu-zhuang-tu-zhong-zui-da-de-ju-xing-by-leetcode-/
-            int[] up = new int[m];
-            int[] down = new int[m];
-
-            Deque<Integer> stack = new LinkedList<Integer>(); // 单调栈
-            for (int i = 0; i < m; i++) {
-                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
-                    stack.pop();
-                }
-                up[i] = stack.isEmpty() ? -1 : stack.peek();
-                stack.push(i);
-            }
-            stack.clear();
-            for (int i = m - 1; i >= 0; i--) {
-                while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
-                    stack.pop();
-                }
-                down[i] = stack.isEmpty() ? m : stack.peek();
-                stack.push(i);
-            }
-
-            for (int i = 0; i < m; i++) {
-                int height = down[i] - up[i] - 1;
-                int area = height * left[i][j];
-                ret = Math.max(ret, area);
-            }
-        }
-        return ret;
+        return -1;
     }
 }
