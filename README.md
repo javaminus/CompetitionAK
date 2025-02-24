047936610536c4b2ab104e1213e1fe1ec3342aee
javaminus <3292158907@qq.com>
2025-02-24 17:05:15 +0800

    2025/2/24

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
 > 算法并不是最难的，因为这事逻辑。 真正困难的地方，是贪婪思维下的隐性决策，那是无法细节预测和探索的。
 
 - 数组不变，区间查询：前缀和、树状数组、线段树；
+
 - 数组单点修改，区间查询：树状数组、线段树；
+
 - 数组区间修改，单点查询：差分、线段树；
+
 - 数组区间修改，区间查询：线段树。
+
 - 看到「最大化最小值」或者「最小化最大值」就要想到二分答案，这是一个固定的套路。
+
 - 为什么？一般来说，二分的值越大，越能/不能满足要求；二分的值越小，越不能/能满足要求，有单调性，可以二分。
+
 - 无权，采用BFS;
   有权，权重均为非负，采用dijkstra;
   有权，权重包含负值，采用bellman-ford.
+
+- 比较两个数字拼接大小排序：
+
+  ```java
+  int n = nums.length;
+          String[] arr = new String[n];
+          for (int i = 0; i < n; i++) {
+              arr[i] = "" + nums[i];
+          }
+          Arrays.sort(arr, (a,b)->{
+              String x = a + b, y = b + a;
+              return y.compareTo(x);
+          });
+  ```
+
 - 48～57为0到9十个阿拉伯数字；65～90为26个大写英文字母，97～122号为26个小写英文字母
+
 - 字符串大小写互换：c^=32
+
 - 判断一个字符是数字：Character.isDigit(c)
+
 - 数位公式：y = x + (s[j] - s[i]) * (POW10[i] - POW10[j]) 交换x的第i位与第j位 
+
 - private static final int[] POW10 = new int[]{1, 10, 100, 1000, 10000, 100000,1000000};
+
 - 二进制一个数是0b开头
+
 - **int**:
   - 范围为 -2^31 到 2^31 - 1，可以表示为 -2 * 10^9 到 2 * 10^9。
+
 - **long**:
   - 范围为 -2^63 到 2^63 - 1，可以表示为 -9 * 10^18 到 9 * 10^18。
+
 - byte: 1 bytes
   short: 2 bytes
   int: 4 bytes
@@ -27,11 +56,18 @@
   float: 4 bytes
   double: 8 bytes
   boolean: 1 bytes (in arrays)
+
 - ![img.png](assets/img.png)
+
 - ![1713108404643](F:\leetcode\README.assets\1713108404643.png)
+
 - ![1716388225535](assets/1716388225535.png)
+
 - ![img.png](assets/img.png)
+
 - ![img_3.png](assets/img_3.png)
+
 - ![img_1.png](assets/img_1.png)
+
 - ![img_2.png](assets/img_2.png)
 
--- src/Solution.java
+++ src/Solution.java
@@ -1,78 +1,25 @@
-class Skiplist {
-    static final int LEVEL_LIMIT = 32;
-    static final int FACTOR = 2;
-    Node pseudoHead;
-    int maxLevel;
+import java.util.List;
 
-    public Skiplist() {
-        pseudoHead = new Node(-1, LEVEL_LIMIT);
-        maxLevel = -1;
+class Solution {
+    public int minLengthAfterRemovals(List<Integer> nums) {
+        int n = nums.size();
+        int x = nums.get(n / 2);
+        int mxCnt = binarySearch(nums, x + 1) - binarySearch(nums, x);
+        System.out.println(binarySearch(nums, x + 1));
+        System.out.println(binarySearch(nums, x));
+        return Math.max(n % 2, mxCnt * 2 - n);
     }
 
-    public boolean search(int target) {
-        Node node = pseudoHead;
-        for (int i = maxLevel; i >= 0; i--) {
-            node = findNodeBefore(i, node, target);
-            if (node.next[i] != null && node.next[i].val == target) {
-                return true;
+    private int binarySearch(List<Integer> nums, int tar) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums.get(mid) <= tar) {
+                l = mid + 1;
+            }else{
+                r = mid - 1;
             }
         }
-        return false;
-    }
-
-    public void add(int num) {
-        int curLevel = getRandomLevel();
-        maxLevel = Math.max(maxLevel, curLevel);
-        Node addNode = new Node(num, curLevel + 1);
-        Node node = pseudoHead;
-        for (int i = maxLevel; i > curLevel; i--) {
-            node = findNodeBefore(i, node, num);
-        }
-        for (int i = curLevel; i >= 0; i--) {
-            node = findNodeBefore(i, node, num);
-            if (node.next[i] != null) {
-                addNode.next[i] = node.next[i];
-            }
-            node.next[i] = addNode;
-        }
-    }
-
-    public boolean erase(int num) {
-        boolean erased = false;
-        Node node = pseudoHead;
-        for (int i = maxLevel; i >= 0; i--) {
-            node = findNodeBefore(i, node, num);
-            if (node.next[i] != null && node.next[i].val == num) {
-                node.next[i] = node.next[i].next[i];
-                erased = true;
-            }
-        }
-        return erased;
-    }
-
-    private Node findNodeBefore(int level, Node start, int target) {
-        Node node = start;
-        while (node.next[level] != null && node.next[level].val < target) {
-            node = node.next[level];
-        }
-        return node;
-    }
-
-    private int getRandomLevel() {
-        int maxLevel = 0;
-        while (maxLevel < LEVEL_LIMIT - 1 && (int) (Math.random() * FACTOR) == 0) {
-            maxLevel++;
-        }
-        return maxLevel;
-    }
-}
-
-class Node {
-    int val;
-    Node[] next;
-
-    public Node(int val, int size) {
-        this.val = val;
-        next = new Node[size];
+        return l - 1;
     }
 }
