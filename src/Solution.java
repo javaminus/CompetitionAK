286f1a75f177a3b7d85bb34ae23f3405470bf1f6
javaminus <3292158907@qq.com>
2025-03-30 14:40:32 +0800

    2025/3/30

--- src/Solution.java
+++ src/Solution.java
@@ -1,71 +1,27 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public long maximumValueSum(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        long[][][] prefixSum = new long[m][3][2]; // 前i行的第j大元素[值， 列]
-        int[][] p = new int[3][2];
-        for (int i = 0; i < 3; i++) {
-            Arrays.fill(p[i], Integer.MIN_VALUE / 2);
-        }
-        for (int i = 0; i < m; i++) {
-            update(grid[i], p);
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 2; k++) {
-                    prefixSum[i][j][k] = p[j][k];
-                }
+    public int maxActiveSectionsAfterTrade(String s) {
+        int n = s.length();
+        List<Integer> list = new ArrayList<>();
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1 || s.charAt(i) != s.charAt(i + 1)) {
+                list.add(i - j + 1);
+                j = i + 1;
             }
         }
-        long[][][] suffixSum = new long[m][3][2];
-        for (int i = 0; i < 3; i++) {
-            Arrays.fill(p[i], Integer.MIN_VALUE / 2);
+
+        int ans = 0;
+        for (int i = (s.charAt(0) == '0' ? 0 : 1); i + 2 < list.size(); i += 2) {
+            ans = Math.max(ans, list.get(i) + list.get(i + 2));
         }
-        for (int i = m - 1; i >= 0; i--) {
-            update(grid[i], p);
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 2; k++) {
-                    suffixSum[i][j][k] = p[j][k];
-                }
-            }
-        }
-        long ans = Long.MIN_VALUE;
-        for (int i = 1; i < m - 1; i++) {
-            for (int b = 0; b < n; b++) {
-                for (long[] a : prefixSum[i - 1]) {
-                    if (a[1] == b) {
-                        continue;
-                    }
-                    for (long[] c : suffixSum[i + 1]) {
-                        if (c[1] == b || c[1] == a[1]) {
-                            continue;
-                        }
-                        ans = Math.max(ans, grid[i][b] + a[0] + c[0]);
-                    }
-                }
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                ans++;
             }
         }
         return ans;
-    }
-
-    private void update(int[] row, int[][] p) {
-        for (int j = 0; j < row.length; j++) {
-            int x = row[j];
-            if (x > p[0][0]) {
-                if (p[0][1] != j) {
-                    if (p[1][1] != j) {
-                        p[2] = new int[]{p[1][0], p[1][1]};
-                    }
-                    p[1] = new int[]{p[0][0], p[0][1]};
-                }
-                p[0] = new int[]{x, j};
-            } else if (x > p[1][0] && j != p[0][1]) {
-                if (p[1][1] != j) {
-                    p[2] = new int[]{p[1][0], p[1][1]};
-                }
-                p[1] = new int[]{x, j};
-            } else if (x > p[2][0] && j != p[0][1] && j != p[1][1]) {
-                p[2] = new int[]{x, j};
-            }
-        }
     }
 }
