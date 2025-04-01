b066da28af1b6065e97be8b26b48d8ef3ba299d1
javaminus <3292158907@qq.com>
2025-04-01 16:48:59 +0800

    2025/4/1

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,223 @@
-import java.util.Scanner;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main { // https://codeforces.com/problemset/problem/546/D
-    static final int MAX = 5000001;  // 最大值，定义求解上限
-    static int[] prime = new int[MAX];  // 用于存储质数数组
-    static int num = 0;  // 质数数量计数器
-    static int[] sum = new int[MAX];  // 前缀和数组，存储每个数的质因子个数累计和
-    static boolean[] isp = new boolean[MAX];  // 布尔数组，用于标记质数候选，true表示可能为质数
+public class Main {
+    private final static int INF = Integer.MAX_VALUE / 2;
+    private final static int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-    // 基本筛法和前缀和计算方法
-    static void getprime(int n) {
-        // 初始化：将2到n-1的所有数标记为质数候选
-        // 在 Java 中，boolean 数组默认值为 false，这里将 true 表示可能为质数
-        for (int i = 2; i < n; i++) {
-            isp[i] = true;
+    static class Read {
+        BufferedReader bf;
+        StringTokenizer st;
+        BufferedWriter bw;
+
+        public Read() {
+            bf = new BufferedReader(new InputStreamReader(System.in));
+            st = new StringTokenizer("");
+            bw = new BufferedWriter(new OutputStreamWriter(System.out));
         }
-        // 筛法过程：计数每个数的不同质因子数量
-        for (int i = 2; i < n; i++) {
-            if (isp[i]) {
-                // 如果 i 是质数，则存入质数数组
-                prime[++num] = i;
-                // 对于质数，其质因子个数为 1
-                sum[i] = 1;
-            }
-            for (int j = 1; j <= num; j++) {
-                // 如果 i 与 prime[j] 的乘积超出范围，则结束循环
-                if ((long)i * prime[j] >= n) {
-                    break;
-                }
-                int index = i * prime[j];
-                // 将该数标记为合数
-                isp[index] = false;
-                // 计算质因子个数：为 i 的质因子个数加 1
-                sum[index] = sum[i] + 1;
-                // 若 i 能被 prime[j] 整除，则跳出循环避免重复计数
-                if (i % prime[j] == 0) {
-                    break;
-                }
-            }
+
+        public String nextLine() throws IOException {
+            return bf.readLine();
         }
-        // 计算前缀和：对于每个 i，sum[i] 为 1 到 i 的质因子个数的累计和
-        for (int i = 1; i < n; i++) {
-            sum[i] += sum[i - 1];
+
+        public String next() throws IOException {
+            while (!st.hasMoreTokens()) {
+                st = new StringTokenizer(bf.readLine());
+            }
+            return st.nextToken();
+        }
+
+        public char nextChar() throws IOException {
+            return next().charAt(0);
+        }
+
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        public float nextFloat() throws IOException {
+            return Float.parseFloat(next());
+        }
+
+        public byte nextByte() throws IOException {
+            return Byte.parseByte(next());
+        }
+
+        public short nextShort() throws IOException {
+            return Short.parseShort(next());
+        }
+
+        public BigInteger nextBigInteger() throws IOException {
+            return new BigInteger(next());
+        }
+
+        public void println(int a) throws IOException {
+            bw.write(String.valueOf(a));
+            bw.newLine();
+            return;
+        }
+
+        public void print(int a) throws IOException {
+            bw.write(String.valueOf(a));
+            return;
+        }
+
+        public void println(String a) throws IOException {
+            bw.write(a);
+            bw.newLine();
+            return;
+        }
+
+        public void print(String a) throws IOException {
+            bw.write(a);
+            return;
+        }
+
+        public void println(long a) throws IOException {
+            bw.write(String.valueOf(a));
+            bw.newLine();
+            return;
+        }
+
+        public void print(long a) throws IOException {
+            bw.write(String.valueOf(a));
+            return;
+        }
+
+        public void println(double a) throws IOException {
+            bw.write(String.valueOf(a));
+            bw.newLine();
+            return;
+        }
+
+        public void print(double a) throws IOException {
+            bw.write(String.valueOf(a));
+            return;
+        }
+
+        public void print(BigInteger a) throws IOException {
+            bw.write(a.toString());
+            return;
+        }
+
+        public void print(char a) throws IOException {
+            bw.write(String.valueOf(a));
+            return;
+        }
+
+        public void println(char a) throws IOException {
+            bw.write(String.valueOf(a));
+            bw.newLine();
+            return;
         }
     }
 
-    public static void main(String[] args) {
-        // 预处理并构建筛法至 MAX
-        getprime(MAX);
+    static class Pair<T, U> {
+        T fir;
+        U sec;
+        public Pair(T fir, U sec) {
+            this.fir = fir;
+            this.sec = sec;
+        }
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        int T = scanner.nextInt();
+    private static long qpow(long a, long b, long p) {
+        long res = 1L;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                res = (res * a) % p;
+            }
+            a = a * a % p;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    private static long sqrt(long N) {
+        long lo = 1;
+        long hi = N;
+        long ans = 0;
+        while(lo <= hi) {
+            long mid = (lo + hi) / 2;
+            if (mid <= N / mid) {
+                ans = mid;
+                lo = mid + 1;
+            }  else {
+                hi = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private static void reverse(char[] s) {
+        int l = 0, r = s.length - 1;
+        while (l <= r) {
+            char tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+
+    private static void reverse(int[] s) {
+        int l = 0, r = s.length - 1;
+        while (l <= r) {
+            int tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+
+    private static void reverse(long[] s) {
+        int l = 0, r = s.length - 1;
+        while (l <= r) {
+            long tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+
+    static Read sc = new Read();
+    private static final int Mod = (int) 1e9 + 7;
+    private static int T = 1;
+
+    public static void main(String[] args) throws IOException {
+        // int T = sc.nextInt();
         while (T-- > 0) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            // 输出 a 与 b 之间前缀和的差值
-            System.out.println(sum[a] - sum[b]);
+            solve();
+            // sc.bw.flush();
         }
-        scanner.close();
+        sc.bw.flush();
+        sc.bw.close();
     }
+
+    private static String[] ss;
+    private static String s;
+    private static char[] cs;
+    private static List<Integer>[] g;
+    private static int m, n, k;
+    private static long[] nums, a, b, left, right, dp, f;
+
+    private static void solve() throws IOException {
+        
+    }
+
+
+
+
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,60 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+
 class Solution {
-    public int closestToTarget(int[] arr, int target) {
-        int n = arr.length;
-        int ans = Integer.MAX_VALUE;
+    public int minimumCost(String target, String[] words, int[] costs) {
+        char[] cs = target.toCharArray();
+        int n = cs.length;
+        final int Mod1 = 1_000_000_007;
+        final int Mod2 = 1_070_777_777;
+        final int Base1 = (int) 8e8 + new Random().nextInt((int) 1e8);
+        final int Base2 = (int) 8e8 + new Random().nextInt((int) 1e8);
+        int[] powBase1 = new int[n + 1];
+        int[] powBase2 = new int[n + 1];
+        int[] preHash1 = new int[n + 1];
+        int[] preHash2 = new int[n + 1];
+        powBase1[0] = powBase2[0] = 1;
         for (int i = 0; i < n; i++) {
-            int x = arr[i];
-            ans = Math.min(ans, Math.abs(arr[i] - target));
-            for (int j = i - 1; j >= 0 && (arr[j] | x) != arr[j]; j--) {
-                arr[j] |= x;
-                ans = Math.min(ans, Math.abs(arr[j] - target));
+            powBase1[i + 1] = (int) ((long) powBase1[i] * Base1 % Mod1);
+            powBase2[i + 1] = (int) ((long) powBase2[i] * Base2 % Mod2);
+            preHash1[i + 1] = (int) (((long) preHash1[i] * Base1 + cs[i]) % Mod1);
+            preHash2[i + 1] = (int) (((long) preHash2[i] * Base2 + cs[i]) % Mod2);
+        }
+        HashMap<Long, Integer> map = new HashMap<>();
+        for (int i = 0; i < costs.length; i++) {
+            long h1 = 0, h2 = 0;
+            for (char c : words[i].toCharArray()) {
+                h1 = (h1 * Base1 + c) % Mod1;
+                h2 = (h2 * Base2 + c) % Mod2;
+            }
+            map.merge(h1 << 32 | h2, costs[i], Integer::min);
+        }
+        HashSet<Integer> lengthSet = new HashSet<>();
+        for (String w : words) {
+            lengthSet.add(w.length());
+        }
+        int[] sortedLens = new int[lengthSet.size()];
+        int k = 0;
+        for (int len : lengthSet) {
+            sortedLens[k++] = len;
+        }
+        Arrays.sort(sortedLens);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE / 2);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j : sortedLens) {
+                if (j > i) {
+                    break;
+                }
+                long subHash1 = ((preHash1[i] - (long) preHash1[i - j] * powBase1[j]) % Mod1 + Mod1) % Mod1;
+                long subHash2 = ((preHash2[i] - (long) preHash2[i - j] * powBase2[j]) % Mod2 + Mod2) % Mod2;
+                long subHash = subHash1 << 32 | subHash2;
+                dp[i] = Math.min(dp[i], dp[i - j] + map.getOrDefault(subHash, Integer.MAX_VALUE / 2));
             }
         }
-        return ans;
+        return dp[n] == Integer.MAX_VALUE / 2 ? -1 : dp[n];
     }
 }
